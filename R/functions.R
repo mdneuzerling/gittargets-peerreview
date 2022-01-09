1bcffe9917af2af4b45cab1f7203b32454675861
mdneuzerling <david@neuzerling.com>
2022-01-09 14:12:45 +1100

    Set up basic matrix work

--- R/functions.R
+++ R/functions.R
@@ -1,7 +1,3 @@
-current_branch <- function() {
-  NULL
-}
-
-matrix_size <- function() {
-  NULL
-}
+# A 10000x10000 matrix of 1s is around 760 MB
+# 1000x1000 is around 7.6 MB
+get_matrix_size <- function() 100
