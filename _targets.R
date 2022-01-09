aa8f225956847ba15657e98328d73f3249a4fdcd
mdneuzerling <david@neuzerling.com>
2022-01-09 14:29:51 +1100

    Add number_elements

--- _targets.R
+++ _targets.R
@@ -4,5 +4,6 @@ source("R/functions.R")
 
 list(
   tar_target(matrix_size, get_matrix_size()),
-  tar_target(big_matrix, matrix(1, matrix_size, matrix_size))
+  tar_target(big_matrix, matrix(1, matrix_size, matrix_size)),
+  tar_target(number_elements, prod(dim(big_matrix)))
 )
